28e9fe66ee13bc632d95ac52188028cbd3f2f340
Christian <christian.schunck10@gmail.com>
2025-11-21 20:50:22 -0300

    tailwind cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./index.html','./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6A00FF',
+        secondary: '#B983FF'
+      }
+    }
+  },
+  darkMode: 'class',
+  plugins: [],
+}
